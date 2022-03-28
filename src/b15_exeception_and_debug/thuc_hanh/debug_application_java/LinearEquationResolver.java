d5dc5506c0e2ee2a92a692b7c26a3c0c9b998505
luongthanhthang <thanhthang2762000@gmail.com>
2022-03-28 21:29:27 +0700

    nop bai tap

---
+++ src/b15_exeception_and_debug/thuc_hanh/debug_application_java/LinearEquationResolver.java
@@ -0,0 +1,30 @@
+package b15_exeception_and_debug.thuc_hanh.debug_application_java;
+
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("b: ");
+        //điều kiện để breakpoint được thực hiện
+        double b = scanner.nextDouble();
+
+        if (a != 0) {
+            double solution = -b / a;
+            System.out.printf("The solution is: %f!", solution);
+        } else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+}
